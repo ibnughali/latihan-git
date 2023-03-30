343fd653c2010e6508e97eb3d5b11495645cc3a6
Ibnu Ghali Alfrizi <128034508+ibnughali@users.noreply.github.com>
2023-03-30 16:54:08 +0700

    CONTOH1

---
+++ contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo"Nama saya Ibnu Ghali Alfarizi saya tinggal dicikarang";
+    }
+}
